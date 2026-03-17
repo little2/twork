5bbf1836cdc7d13faeccb21d7731c98bd4ecec23
little2 <agile@little2.net>
2026-03-17 23:37:21 +0800

    update

--- reset.py
+++ reset.py
@@ -12,8 +12,9 @@ import pymysql
 # Check if running in a local development environment
 if not os.getenv('GITHUB_ACTIONS'):
     from dotenv import load_dotenv
+    load_dotenv(dotenv_path='.20100034.sungfong.env')
     # load_dotenv(dotenv_path='.28817994.get_account.env')
-    load_dotenv(dotenv_path='.25195066.get_account.env')
+    # load_dotenv(dotenv_path='.25195066.get_account.env')
 
     # load_dotenv(dotenv_path='.20100034.sungfong.env')
     # load_dotenv(dotenv_path='.24066130.decode.env')
